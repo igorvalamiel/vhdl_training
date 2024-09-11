c3bcb5a8ef3193fa9d47c35bfb28dd1396706a91
Igor Valamiel Peixoto Nunes <igorvpnunes05@gmail.com>
2024-09-11 11:51:44 -0300

    update 2

--- machine.vhd
+++ machine.vhd
@@ -1,29 +1,59 @@
--- tentando adiantar o código para o lab de SD
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:24:43 09/10/2024 
+-- Design Name: 
+-- Module Name:    machine - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity machine is
-    port(
-        keys    : in bit_vector (3 downto 0);
-        clock   :   in bit;
-        leds  :   out bit_vector (3 downto 0);
-        flag_zero   :   out bit;
-        flag_sign   :   out bit;
-        flag_overf  :   out bit;
-        flag_cout   :   out bit
-    );
-end entity;
+    Port ( keys : in  std_logic_vector (3 downto 0);
+           leds : out  std_logic_vector (3 downto 0);
+           clock : in  bit;
+           flag_zero : out  bit;
+           flag_sign : out  bit;
+           flag_overf : out  bit;
+           flag_cout : out  bit);
+end machine;
 
 architecture main of machine is
-    process
+	 
+	 -- declaring variables
+        signal state: std_logic_vector(1 downto 0) := "00";
+        signal operation: std_logic_vector(3 downto 0) := "0000";
+        signal val1: std_logic_vector(3 downto 0) := "0000";
+        signal val2: std_logic_vector(3 downto 0) := "0000";
+        signal trash_4: std_logic_vector(3 downto 0) := "0000";
+        signal trash_5: std_logic_vector(4 downto 0) := "00000";
+		  signal num1: integer;
+begin	 
+	 
+	 process (clock)
+	 
     begin
-        -- declaring variables
-        signal state: bit_vector(1 downto 0) := "00";
-        signal operation: bit_vector(3 downto 0) := "0000";
-        signal val1: bit_vector(3 downto 0) := "0000";
-        signal val2: bit_vector(3 downto 0) := "0000";
-        signal trash_4: bit_vector(4 downto 0) := "0000";
-        signal trash_5: bit_vector(4 downto 0) := "00000";
-
-        wait until clock'event and clock='1';
         --Input operation
         if state = "00" then
             operation <= keys;
@@ -32,61 +62,43 @@ architecture main of machine is
         --Input value 1
         elsif state = "01" then
             val1 <= keys;
-            --dealing with exceptions
-            if (operation = "0001") or (operation = "0100") or (operation = "0101") or (operation = "1000") then 
-                --One value only operation execution
-                case operation is
-
-                    --inverter
-                    when "0001" =>
-                        val1 <= not val1;
-                        if val1 = "0000" then flag_zero = '1'; end if;
-                        leds <= val1;
-                        --????? negative????
-                    
-                    --shift
-                    when "0100" =>
-                        if val1(3) = '0' then
-                            flag_overf <= '1';
-                        elsif val1(3) = '1' then
-                            flag_cout <= '1';
-                        end if;
-                        val1 <= val1 + 2;
-                        leds <= val1;
-                        if val1 = "0000" then 
-                            flag_zero <= '1';
-                        end if;
-                end case;
+			  --One value only operation execution
+			  --inverter
+			  if operation = "0001" then
+					val1 <= not val1;
+					if val1 = "0000" then flag_zero <= '1'; end if;
+					leds <= val1;
+			  
+			  --shift
+			  elsif operation = "0100" then
+					if val1(3) = '0' then
+						 flag_overf <= '1';
+					elsif val1(3) = '1' then
+						 flag_cout <= '1';
+					end if;
+					--num1 <= to_integer(val1) + 2;
+					leds <= std_logic_vector(unsigned(val1) + 2);
+					if val1 = "0000" then 
+							 flag_zero <= '1';
+					end if;
                 state <= "11";
-            else state <= "10";
-            end if;
+					else state <= "10";
+				end if;
             
         --Input value 2
         elsif state = "10" then
             val2 <= keys;
-            state <= '11';
-            case operation is
+            state <= "11";
 
                 -- Addition
-                when "0010" =>
-                    trash_5 <= val1 + val2; -- completar as val com 0 no mais significativo
-                    if trash_5(4) = '1' then
-                        flag_cout <= '1';
-                        flag_sign <= '1';
-                    end if;
-                    trash_5(4) <= '0';
-                    trash(5) <= (not trash_5) + "00001"
-                    trash_4 <= trash_5(3 downto 0);
-                    if trash_5(4) = '1' then
-                        flag_overf <= '1';
-                    end if;
-                    leds <= trash_4;
+                --if operation = "0010" then
                 
                 -- Subtraction
-                when "0011" =>
+                --if operation = "0011" then
+                    
                 
                 -- Greater
-                when "0110" =>
+                if operation = "0110" then
                     if (val1 > val2) then
                         leds <= val1;
                     else
@@ -94,23 +106,21 @@ architecture main of machine is
                     end if;
                 
                 -- Smaller
-                when "0111" =>
+                elsif operation = "0111" then
                     if (val1 < val2) then
                         leds <= val1;
                     else
                         leds <= val2;
                     end if;
-            end case;
+					end if;
         
         --clear state
         else
             leds <= "0000";
-            extra <= "0000";
             state <= "00";
             operation <= "0000";
         end if;
     end process;
 end main;
-
 
 
