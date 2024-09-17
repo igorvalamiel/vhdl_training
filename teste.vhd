de32bb996ec8b3e17f73b20355778a89fb493ffb
Igor Valamiel Peixoto Nunes <igorvpnunes05@gmail.com>
2024-09-16 21:32:16 -0300

    teste

---
+++ teste.vhd
@@ -0,0 +1,5 @@
+if operation = "0010" then
+    c0 <= val1(0)*val2(0);
+    c1 <= c0(val1(1)+val2(1)) + val1(1)*val2(1);
+    c2 <= c0(val1(2)+val2(2)) + val1(2)*val2(2);
+    c3 <= c0(val1(3)+val2(3)) + val1(3)*val2(3);
